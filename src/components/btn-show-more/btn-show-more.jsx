6d126704ffbf68cc57ed07846b5d0f7295be3e21
Neikan <sladkov.e.m@outlook.com>
2020-07-23 17:59:05 +0300

    Refactor BtnShowMore-component

--- src/components/btn-show-more/btn-show-more.jsx
+++ src/components/btn-show-more/btn-show-more.jsx
@@ -1,24 +1,39 @@
-import React from "react";
+import React, {PureComponent} from "react";
 import PropTypes from "prop-types";
 
 
-const handleSelect = (onSelect) => (evt) => {
-  evt.preventDefault();
-  onSelect();
-};
+class BtnShowMore extends PureComponent {
+  constructor(props) {
+    super(props);
+
+    this._handleSelect = this._handleSelect.bind(this);
+  }
 
 
-const BtnShowMore = (props) => {
-  return (
-    <div className="catalog__more">
-      <button className="catalog__button" type="button"
-        onClick={handleSelect(props.onSelect)}
-      >
-        Show more
-      </button>
-    </div>
-  );
-};
+  /**
+   * Метод, обеспечивающий отрисовку компонента
+   * @return {Object} созданный компонент
+   */
+  render() {
+    return (
+      <div className="catalog__more">
+        <button className="catalog__button" type="button"
+          onClick={this._handleSelect}
+        >
+          Show more
+        </button>
+      </div>
+    );
+  }
+
+
+  /**
+     * Метод, обспечивающий выполнение помощника для отрисовки дополнительных фильмов
+     */
+  _handleSelect() {
+    this.props.onSelect();
+  }
+}
 
 
 BtnShowMore.propTypes = {
